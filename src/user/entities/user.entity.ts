6d5f475a33c6195b1456b276e9a057b33533749d
nagestech <nagestechcat@gmail.com>
2023-12-10 22:24:54 +0530

    mysql-nest connected

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,21 @@
+import {Entity,Column,PrimaryGeneratedColumn} from 'typeorm';
+
+@Entity()
+export class User {
+@PrimaryGeneratedColumn()
+id:number;
+
+@Column()
+firstname:string;
+
+
+
+@Column()
+lastname:string;
+
+@Column()
+email:string;
+
+@Column()
+password:string;
+}
